2b08f50956d6ccbcb591b1934e1d64dd3858ec86
Ryan Cohrs <ryan.cohrs@henrycountyohio.gov>
2020-02-06 15:36:09 -0500

    commit

--- print.py
+++ print.py
@@ -1,8 +1,12 @@
-character_name = "Ryan"
-character_age = 25
-is_male = True
+company_name = "Cohrs Inc."
+openings = "IT, Accounting, HR."
+openings_number = 3
 
-print(character_name + " is the best IT.")
-print("Everyone likes his work ethic.")
-print("Everyone likes " + character_name + ".")
-print(character_name + " is " + character_age + " years old.")
+print(company_name + " is currently hiring the following positions:")
+print(openings)
+###Converting Numbers to strings
+print("Which is " + str(openings_number) + " openings.")
+###Amount of chacters in string.
+print(len(openings))
+print(company_name)
+print(openings_number)
