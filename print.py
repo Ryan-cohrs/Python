e51dd1c47d823d629b029498c0cc47f5f8ead378
Ryan Cohrs <ryan.cohrs@henrycountyohio.gov>
2020-02-06 11:24:21 -0500

    commit

---
+++ print.py
@@ -0,0 +1,8 @@
+character_name = "Ryan"
+character_age = 25
+is_male = True
+
+print(character_name + " is the best IT.")
+print("Everyone likes his work ethic.")
+print("Everyone likes " + character_name + ".")
+print(character_name + " is " + character_age + " years old.")
