84441e4b5f00ac717cd0918a47c2a5eed49db5df
Ryan Cohrs <ryan.cohrs@henrycountyohio.gov>
2020-02-06 15:54:43 -0500

    commit

--- math.py
+++ math.py
@@ -1,3 +1,12 @@
+from math import *
+
 num1 = 5
 num2 = 10
+###Absolute value
 print(abs(num1))
+###To the power of
+print(pow(num1, num2))
+###Min and Max
+print(min(num1, num2))
+print(max(num1, num2))
+print(sqrt(256))
