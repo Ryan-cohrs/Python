9de719fb9f5fb2e10188ec6861aa6f16a56f1c17
Ryan Cohrs <ryan.cohrs@henrycountyohio.gov>
2020-02-07 08:27:39 -0500

    commit

---
+++ list.py
@@ -0,0 +1,5 @@
+data = ["Ryan", 25, "IT Technician"]
+name = data[0]
+age = data[1]
+job = data[2]
+print("You're name is " + name + " and you are " + str(age) + " years old and you work as an " + job + ".")
