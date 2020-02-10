946eca34df83efc65a713b61c49c4f5d5549219a
Ryan Cohrs <ryan.cohrs@henrycountyohio.gov>
2020-02-10 08:18:15 -0500

    Update list.py

--- list.py
+++ list.py
@@ -1,5 +1,6 @@
-data = ["Ryan", 25, "IT Technician"]
+info = input("Please enter your name, age, and job title:  ")
+data = info.split()
 name = data[0]
 age = data[1]
 job = data[2]
-print("You're name is " + name + " and you are " + str(age) + " years old and you work as an " + job + ".")
+print("You're name is " + name + " and you are " + age + " years old and you work as an " + job + ".")
